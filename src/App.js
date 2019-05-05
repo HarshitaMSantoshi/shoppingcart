c36253b048586c1d09a2af58a718d96ff3702c3e
HarshitaMSantoshi <harshitamsantoshi93@gmail.com>
2019-05-05 19:07:51 +0530

    Fetching data and setting state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import style from './App.css';
+import styles from './App.css';
 import Navbar from './components/Navbar';
 import ItemsList from './components/ItemsList';
 import Cart from './components/Cart';
+import Axios from 'axios';
 
 class App extends Component {
   state = {
-    activeTab: 0
+    activeTab: 0,
+    items: []
   };
+
+  componentDidMount () {
+    Axios.get('http://my-json-server.typicode.com/4d4rsh/mock-shopping-site-data/items')
+      .then(response => this.setState({
+        items: response.data
+      }));
+  }
 
   handleTabChange = index => {
     this.setState({
@@ -28,9 +37,9 @@ class App extends Component {
     const { activeTab } = this.state;
     return (
       <div className="App">
-        <div className={style.app}>
+        <div className={styles.app}>
           <Navbar activeTab={activeTab} onTabChange={this.handleTabChange} />
-          <main className={style.appContent}>
+          <main className={styles.appContent}>
             {this.renderContent()}
           </main>
         </div>
