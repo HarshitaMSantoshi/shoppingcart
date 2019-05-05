8c47fa4804122d75854469cb5f8b358baa3cbff2
HarshitaMSantoshi <harshitamsantoshi93@gmail.com>
2019-05-05 17:47:20 +0530

    Rendering component based on nav selection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import style from './App.css';
 import Navbar from './components/Navbar';
+import ItemsList from './components/ItemsList';
+import Cart from './components/Cart';
 
 class App extends Component {
   state = {
@@ -13,12 +15,24 @@ class App extends Component {
     });
   }
 
+  renderContent = () => {
+    const { activeTab } = this.state;
+    switch (activeTab) {
+      default:
+      case 0: return <ItemsList />;
+      case 1: return <Cart />
+    }
+  }
+
   render() {
     const { activeTab } = this.state;
     return (
       <div className="App">
         <div className={style.app}>
-          <Navbar activeTab={activeTab} onTabChange={this.handleTabChange}/>
+          <Navbar activeTab={activeTab} onTabChange={this.handleTabChange} />
+          <main className={style.appContent}>
+            {this.renderContent()}
+          </main>
         </div>
       </div>
     );
